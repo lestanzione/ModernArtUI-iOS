bec28075417d4ad61715f45a8c07376839d3effb
Leandro <leandro@Leandros-Mac.local>
2015-02-05 16:06:33 -0800

    Initial Commit

---
+++ ModernArtUI-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModernArtUI-iOS
+//
+//  Created by Leandro on 2/5/15.
+//  Copyright (c) 2015 Leandro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
